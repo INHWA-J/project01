8247176a2e573f38f9915e5760a3e7e36c76e150
INHWA JOO <snow09617@gmail.com>
2023-08-31 23:14:20 +0900

    230831 first commit

---
+++ src/main/java/com/example/animal/AnimalApplication.java
@@ -0,0 +1,14 @@
+package com.example.animal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(AnimalApplication.class, args);
+		System.out.println("test합니다.");
+	}
+
+}
